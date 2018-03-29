2e9e039611f3fa904bdce184443d6496a73b271c
Michelle Olivieri <michelleo72@gmail.com>
2018-03-29 00:25:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ attr_accessor :name, :artist
     self.artist = Artist.find_or_create_by_name(name)
       artist.add_song(self)
 
-binding.pry
-      
+      binding.pry
+
   end
 
   def self.new_by_filename(file_name)
