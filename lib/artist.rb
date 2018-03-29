71f19a8750a4568875b40dcc8b37ff94982fee1c
Michelle Olivieri <michelleo72@gmail.com>
2018-03-29 00:23:06 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+
+attr_accessor :name, :songs
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.detect do |artist_name|
+      self.new(name) if artist_name != name
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name}
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Song
+
+attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+
+      artist.add_song(self)
+
+  end
+
+  def self.new_by_filename(file_name)
+    file = file_name.split(" - ")
+      song = self.new(file[1])
+      artist_name=(file[0])
+song.artist = artist_name
+
+      song
+  end
+
+end
